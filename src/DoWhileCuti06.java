b86cd3b70312204de0839cf4747e5e1ca12c0e6a
AJULUMEL <azruljoss@gmail.com>
2023-12-19 22:19:54 +0700

    jobsheet7

---
+++ src/DoWhileCuti06.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class DoWhileCuti06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String Konfirmasi;
+
+        System.out.print("Jatah Cuti: ");
+        jatahCuti = sc.nextInt();
+
+        do {
+            System.out.print("Apakah anda ingin mengambil cuti (y/t)? ");
+            Konfirmasi = sc.next();
+
+            if (Konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah Hari: ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                } else {
+                    System.out
+                            .println("Sisa jatah cuti anda tidak mencukupi,\napakah anda ingin mengisi ulang (y/n)? ");
+                    Konfirmasi = sc.next();
+                    if (!Konfirmasi.equalsIgnoreCase("y"))
+                        break;
+                }
+            } else {
+                break;
+            }
+        } while (jatahCuti > 0);
+
+        sc.close();
+    }
+}
---
+++ src/ForKelipatan06.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class ForKelipatan06 {
+    public static void main(String[] args) throws Exception {
+        Scanner scan = new Scanner(System.in);
+        
+        int kelipatan, jumlah = 0, counter= 0;
+
+        System.out.print("Masukkan bilangan kelipatan (1-9): ");
+        kelipatan = scan.nextInt();
+
+        scan.close();
+
+        for (int i = 1; i <= 50; i++) {
+            if (i % kelipatan == 0) {
+                jumlah += i;
+                counter++;
+            }
+        }
+        double ratarata;
+
+        if (counter != 0) {
+            ratarata = (double) jumlah / counter;
+        } else {
+            ratarata = 0;
+        }
+        scan.close();
+        System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah %d\n", kelipatan, counter);
+        System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah %d\n", kelipatan, jumlah);
+        System.out.printf("Rata-rata bilangan kelipatan %d dari 1-50 adalah %2f\n", kelipatan, ratarata);
+    }
+}
